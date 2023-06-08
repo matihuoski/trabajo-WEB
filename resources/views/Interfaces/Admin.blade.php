3180b35c497ac46ab4e75ebbbebd144f3c17ce0f
matihuoski <matihuoski@gmail.com>
2023-06-08 17:58:42 -0400

    version 4?

--- resources/views/Interfaces/Admin.blade.php
+++ resources/views/Interfaces/Admin.blade.php
@@ -2,5 +2,144 @@
 
 @section('contenido-principal')
 
+    <div class="container-fluid" style="margin-top: 20px">
+        <div class="container-fluid" style="margin-top: 20px">
+            <h2>Profesores</h2>
+            <div class="col-12 col-lg-8 order-last order-lg-first">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Rut</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th colspan="2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="text-center" style="width: 1rem">
+                                <!-- BORRAR -->
+                                <span data-bs-toggle="tooltip" data-bs-title="Borrar #">
+                                    <button type="button" class="btn bt-sm btn-danger pb-0" data-bs-toggle="modal" data-bs-target="#equipoBorrarModal#">
+                                        <span class="material-icons">Borrar</span>
+                                    </button>
+                                </span>
+                            </td>
+                            <td class="text-center" style="width: 1rem">
+                                <a href="#" class="btn btn-sm btn-warning pb-0 text-white" data-bs-toggle="tooltip"
+                                    data-bs-title="Editar #">
+                                    <span class="material-icons">Actualizar</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <!-- Modal Borrar Equipo -->
+                        <div class="modal fade" id="equipoBorrarModal#" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmar Borrar Equipo</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ¿Desea borrar al equipo #?
+                                </div>
+                                <div class="modal-footer">
+                                    <form action="#" method="POST">
+                                        @csrf
+                                        @method('delete') 
+                                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cerrar</button>
+                                        <button type="submit" class="btn btn-danger">Borrar Equipo</button>
+                                    </form>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-        
+        <div class="container-fluid" style="margin-top: 20px">
+            <h2>Alumnos</h2>
+            <div class="col-12 col-lg-8 order-last order-lg-first">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Rut</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Email</th>
+                            <th colspan="2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="text-center" style="width: 1rem">
+                                <!--BORRAR-->
+                                <span data-bs-toggle="tooltip" data-bs-title="Borrar #">
+                                    <button type="button" class="btn bt-sm btn-danger pb-0" data-bs-toggle="modal" data-bs-target="#equipoBorrarModal#">
+                                        <span class="material-icons">Borrar</span>
+                                    </button>
+                                </span>
+                            </td>
+                            <td class="text-center" style="width: 1rem">
+                                <a href="#" class="btn btn-sm btn-warning pb-0 text-white" data-bs-toggle="tooltip"
+                                    data-bs-title="Editar #">
+                                    <span class="material-icons">Actualizar</span>
+                                </a>
+                            </td class="text-center">
+                            
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="container-fluid" style="margin-top: 20px">
+            <h2>Propuestas</h2>
+            <div class="col-12 col-lg-8 order-last order-lg-first">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Fecha</th>
+                            <th>Documento</th>
+                            <th>Rut Estudiante</th>
+                            <th>Estado</th>
+                            <th colspan="2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <tr>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="text-center" style="width: 1rem">
+                                <!--BORRAR-->
+                            </td>
+                            <td class="text-center" style="width: 1rem">
+                                <a href="#" class="btn btn-sm btn-warning pb-0 text-white" data-bs-toggle="tooltip"
+                                    data-bs-title="Editar #">
+                                    <span class="material-icons">Actualizar</span>
+                                </a>
+                            </td class="text-center" style="width: 1rem">
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+
+@endsection
--- resources/views/Interfaces/Profesor.blade.php
+++ resources/views/Interfaces/Profesor.blade.php
@@ -2,3 +2,71 @@
 
 @section('contenido-principal')
 
+<div class="container-fluid" style="margin-top: 20px">
+    <div class="row">
+        <div class="col-lg-8 order-lg-first">
+            <h2>Propuestas</h2>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Fecha</th>
+                            <th>Documento</th>
+                            <th>Estado</th>
+                            <th>RUT Estudiante</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="align-middle">#</td>
+                            <td class="text-center" style="width: 1rem">
+                                <!--BORRAR-->
+                                <span data-bs-toggle="tooltip" data-bs-title="Borrar #">
+                                    <button type="button" class="btn btn-sm btn-danger pb-0" data-bs-toggle="modal" data-bs-target="#equipoBorrarModal#">
+                                        <span class="material-icons">Borrar Comentario</span>
+                                    </button>
+                                </span>
+                            </td>
+                            <td class="text-center" style="width: 1rem">
+                                <a href="#" class="btn btn-sm btn-warning pb-0 text-white" data-bs-toggle="tooltip"
+                                    data-bs-title="Editar #">
+                                    <span class="material-icons">Poner Comentario</span>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Datos Personales</h5>
+                    <form>
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" id="nombre" placeholder="Ingrese su nombre">
+                        </div>
+                        <div class="mb-3">
+                            <label for="apellido" class="form-label">Apellido</label>
+                            <input type="text" class="form-control" id="apellido" placeholder="Ingrese su apellido">
+                        </div>
+                        <div class="mb-3">
+                            <label for="rut" class="form-label">Rut</label>
+                            <input type="text" class="form-control" id="rut" placeholder="Ingrese su rut">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Ingresar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
